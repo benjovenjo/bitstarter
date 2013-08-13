be2dd1c71324bb67592c5e1545e0e77f3eb6d6d4
Ben Dromey <ubuntu@ip-172-31-44-219.us-west-2.compute.internal>
2013-08-13 15:14:52 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
   var file = fs.readFileSync('index.html')
-  response.send(file);
+  response.send(file.toString);
 });
 
 var port = process.env.PORT || 5000;
