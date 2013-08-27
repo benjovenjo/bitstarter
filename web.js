032a0ed230d9aeface8b83e4511f505e83a9f052
benjovenjo <onawintersnight@gmail.com>
2013-08-27 16:46:31 +0000

    Edited the title in index.html

--- web.js
+++ web.js
@@ -1,14 +1,18 @@
+#!/usr/bin/env node
+   
 var express = require('express');
-var app = express();
 var fs = require('fs');
-app.use(express.logger());
+
+var app = express(express.logger());
+
+var buffer = new Buffer(fs.readFileSync('index.html', 'utf-8'), 'utf-8');
+var allLines = buffer.toString('utf-8');
 
 app.get('/', function(request, response) {
-   var file = fs.readFileSync('index.html')
-  response.send(file.toString());
+    response.send(allLines);	
 });
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+}); 
